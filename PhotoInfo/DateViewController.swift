937290b8985bab841957fec232a2177a18d2b5dc
PGMY <PGMYURI@gmail.com>
2014-06-27 15:52:01 +0900

    add gitignore

---
+++ PhotoInfo/DateViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DateViewController.swift
+//  PhotoInfo
+//
+//  Created by Mika Yamamoto on 2014/06/27.
+//  Copyright (c) 2014年 Mika Yamamoto. All rights reserved.
+//
+
+import Foundation
---
+++ PhotoInfo/MYRootViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MYRootViewController.swift
+//  PhotoInfo
+//
+//  Created by Mika Yamamoto on 2014/06/27.
+//  Copyright (c) 2014年 Mika Yamamoto. All rights reserved.
+//
+
+import Foundation
